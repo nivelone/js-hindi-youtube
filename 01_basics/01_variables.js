6caa6fc9ea82fce2c97e078332358bcdc7a1b0b7
AmanSinghBhati <118063825+nivelone@users.noreply.github.com>
2025-12-17 19:43:26 +0000

    learned about converstion part 1

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountEmail = "hitesh"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+let accountState;
+
+// accountId = 2 // Not allowed
+console.log(accountId) 
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId)
+
+console.table([accountEmail,accountPassword,accountCity, accountState])
---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,33 @@
+let score= undefined
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// 33 => number
+// "33abc" => NaN
+// true => 1, false => 0
+
+let isLoggedIn="aman";
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(typeof isLoggedIn);
+
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true, 2=> false
+//""=> false
+//"aman"=> true
+
+let someNumber=33
+console.log(typeof someNumber)
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber);
+
+
+
+
